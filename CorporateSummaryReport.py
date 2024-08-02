8f24f6826139adb1c15df0d9f457bbbdc2f82b95
defford <defford@gmail.com>
2024-08-02 10:11:01 -0230

    Added Corporate Summary Report, fixed minor changes on data files, added a functions file

---
+++ CorporateSummaryReport.py
@@ -0,0 +1,159 @@
+# Title: HAB Taxi Services - Corporate Summary Report Generator
+# Description: Generates summary metrics across company revenue, expense, and employee data for business analytical processes.
+# Date: 2024-07-31 - 2024-08-05
+
+# Import libraries
+import datetime
+from functions import PPrint
+# Define Functions
+
+def LoadToList(file):
+        
+    f = open(file, "r")
+    fRead = f.read()
+    fList = fRead.split("\n")
+    fRecords = []
+    for item in fList:
+        record = item.split(", ")
+        fRecords.append(record)
+
+    return fRecords
+
+# Read all relevant tables into memory as lists to be used for calculations and output.
+revenueList = LoadToList("Data_Files/Revenues.dat")
+
+expensesList = LoadToList("Data_Files/Expenses.dat")
+
+expenseItemsList = LoadToList("Data_Files/ExpenseItems.dat")
+
+employeesList = LoadToList("Data_Files/Employees.dat")
+
+driverDetailsList = LoadToList("Data_Files/DriverDetails.dat")
+
+# Set up Counters and Accumulators
+
+revAcc = 0 # total revenue
+revCtr = 0
+expAcc = 0
+expCtr = 0
+balAcc = 0
+drvCtr = 0
+
+dateStart = "2024-07-01" # input("Enter the first date the report should begin on (YYYY-MM-DD): ")
+dateEnd = "2024-07-15" # input("Enter the date the report should end on (YYYY-MM-DD): ")
+dateStart = datetime.datetime.strptime(dateStart, "%Y-%m-%d").date()
+dateEnd = datetime.datetime.strptime(dateEnd, "%Y-%m-%d").date()
+
+for item in revenueList:
+    date = item[1]
+    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    if date >= dateStart and date <= dateEnd:
+        revCtr += 1
+        revAcc += float(item[-1])
+
+for item in expensesList:
+    date = item[2]
+    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    if date >= dateStart and date <= dateEnd:
+        expCtr += 1
+        expAcc += float(item[-1])
+
+netProfit = revAcc - expAcc
+
+for item in driverDetailsList:
+    balAcc += float(item[-1])
+
+drvCtr = len(employeesList)
+
+avgRev = round(revAcc / drvCtr, 2)
+
+# Store grouped data to determine best revenue item
+groupRev = {}
+
+for item in revenueList:
+    revDate = item[1]
+    revDate = datetime.datetime.strptime(revDate, "%Y-%m-%d").date()
+
+    if revDate >= dateStart and revDate <= dateEnd:
+        revType = item[2]
+        revAmt = float(item[4])
+
+        if revType not in groupRev:
+            groupRev[revType] = 0
+        groupRev[revType] += revAmt
+
+# Determine which item generated the most revenue
+maxRev = 0
+maxRevType = ""
+
+for revType in groupRev:
+    if groupRev[revType] > maxRev:
+        maxRevType = revType
+        maxRev = groupRev[revType]
+
+# Store grouped expense names and cost data by invoice #
+groupExp = {}
+filteredInvNo = []
+
+for item in expensesList:
+    expDate = item[2]
+    expDate = datetime.datetime.strptime(expDate, "%Y-%m-%d").date()
+    expInvNo = item[0]
+
+    if expDate >= dateStart and expDate <= dateEnd:
+        if expDate not in filteredInvNo:
+            filteredInvNo.append(expInvNo)
+
+
+for item in expenseItemsList:
+    invoiceNo = item[1]
+
+    if invoiceNo in filteredInvNo:
+        expType = item[2]
+        expAmt = float(item[-1])
+
+        if invoiceNo not in groupExp:
+            groupExp[invoiceNo] = [expType, expAmt]
+        else:    
+            groupExp[invoiceNo][1] += float(expAmt)
+
+maxExp = float(0)
+maxExpType = ""
+
+for expType in groupExp:
+    if groupExp[expType][1] > maxExp:
+        maxExpType = groupExp[expType][0]
+        maxExp = groupExp[expType][1]
+
+
+print(maxRevType, maxExpType)
+
+
+# Display the Report
+print()
+print()
+PPrint(90, ("HAB Taxi Services - Corporate Summary Report", "center"))
+print()
+PPrint(90, (f"{'-' * 70}", "center"))
+PPrint(90, ("Report Period:", "center"))
+PPrint(90, (f"{dateStart} - {dateEnd}", "center"))
+PPrint(90, (f"{'-' * 70}", "center"))
+print()
+PPrint(90, ("Total Revenue:", 11, "left"), (f"${revAcc:,.2f}", 55))
+PPrint(90, ("Total Expenses:", 11, "left"), (f"${expAcc:,.2f}", 55))
+PPrint(90, ("Net Profit:", 11, "left"), (f"${netProfit:,.2f}", 55))
+print()
+PPrint(90, ("Average Revenue:", 11, "left"), (f"${avgRev:,.2f}", 55))
+PPrint(90, ("Total Owed:", 11, "left"), (f"${balAcc:,.2f}", 55))
+PPrint(90, ("Total Drivers:", 11, "left"), (f"{drvCtr}", 55))
+print()
+PPrint(90, ("Most Revenue by Type:", 11, "left"), ("Most Expensive by Type:", 55))
+PPrint(90, (f"{maxRevType}", 11, "left"), (f"{maxExpType}", 55))
+PPrint(90, (f"${maxRev:,.2f}", 11, "left"), (f"${maxExp:,.2f}", 55))
+print()
+PPrint(90, (f"{'-' * 70}", "center"))
+print()
+PPrint(90, ("Have a nice day!", "center"))
+print()
+
+
---
+++ functions.py
@@ -0,0 +1,116 @@
+import math
+import datetime
+import time
+import sys
+
+def sumPrevClaims(prevClaims):
+    sum = 0
+    
+    for claim in prevClaims:
+        sum += claim
+    
+    return sum
+
+
+def PPrint(screenWidth, *args):
+    """
+    Stands for 'Positional Print'
+    Positions multiple text elements along a line.
+
+    *args allows functions to take arguments as they're given: 
+        Earch arg is a tuple of variable length. Each tuple contains:
+            - content (str): The string to print.
+            - position (int or str): The position along the line if an int, or one of 'left', 'right', 'center'.
+            - align (str, optional): If position is an int, align can be 'left' or 'right'. Defaults to 'left'.
+
+    Returns the elements along a line specifically positioned.
+    """
+
+    line = [' '] * screenWidth  # Creates a list of spaces to build the line
+
+    for content, position, *optional in args:
+        if content: 
+            align = optional[0] if optional else 'left'
+            if isinstance(position, int): 
+                start = position if align == 'left' else position - len(content)
+            elif position == 'left':
+                start = 0
+            elif position == 'right':
+                start = screenWidth - len(content)
+            elif position == 'center':
+                start = (screenWidth - len(content)) // 2
+
+            # Insert the content into the line list
+            for i, char in enumerate(content):
+                if 0 <= start + i < screenWidth:
+                    line[start + i] = char
+        else:
+            print() 
+
+
+    # Convert list back to string and print
+    print(''.join(line))
+
+
+
+def FirstNextMonth(date):
+    day = 1
+    month = date.month
+    year = date.year
+
+    # Calculate the new month and year
+    if month < 12:
+        month += 1
+
+    elif month >= 12:
+        month += 1
+        month -= 12
+        year += 1
+
+    newDate = datetime.datetime(year, month, day)
+    
+    return newDate
+
+def WaitSave(wait):
+    time.sleep(wait)
+    print("SAVING...")
+
+
+# Saving is tied to the progress bar so that both are done at the same time
+def ProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd, flush=True)
+    if iteration == total:
+        print()
+
+def SaveToFileBar(items, filePath, filePathAlias):
+    total_items = len(items)
+    prefix = 'Saving...'
+    suffix = 'Complete'
+    length = 50
+
+    with open(filePath, "a") as filePathAlias:
+        for i, item in enumerate(items, start=1):
+            if i == total_items:
+                filePathAlias.write(f"{item}")
+            else:
+                filePathAlias.write(f"{item}, ")
+            ProgressBar(i, total_items, prefix=prefix, suffix=suffix, length=length)
+            time.sleep(0.1)
+
+def WaitingMessage(content, space):
+
+    ogContent = content
+
+    seconds = 10
+    for dot in range(1, seconds + 1):
+        sys.stdout.write('\r' + (" " * space) + content)
+        sys.stdout.flush()
+
+        time.sleep(0.2)
+        content += "."
+
+        if len(content) > len(ogContent) + 3:
+            content = ogContent
